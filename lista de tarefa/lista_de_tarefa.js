8c8d3b9c071769236d2d4a63f4f11156ee8434ec
emerson da conceição cabral <emersoncabral202@gmail.com>
2021-06-14 22:16:15 -0300

    ajustes de função

--- lista de tarefa/lista_de_tarefa.js
+++ lista de tarefa/lista_de_tarefa.js
@@ -1,18 +1,24 @@
 var texto_tarefa = document.querySelector(".texto_tarefa")
 var conteiner_lista = document.querySelector(".conteiner_lista")
-var tarefa = document.getElementById("tarefa")
+var lista = document.getElementsByName("lista")
+var tarefa = document.getElementsByName("tarefa")
 
-
+var numlista = 0
 
 var adicionar = document.querySelector(".adicionar").addEventListener("click", function (){
-    conteiner_lista.innerHTML += '<div class="lista"><div class="tarefa" id="tarefa">' + texto_tarefa.value + '</div><button class="concluida" onclick="concluida(this)><i class="fas fa-check"></i></button><button class="excluir"><i class="fas fa-times"></i></button></div>'
+    conteiner_lista.innerHTML += '<div class="lista" name="lista"><div class="tarefa" name="tarefa">' + texto_tarefa.value + '</div><button class="concluida" onclick="concluida('+ numlista +')"><i class="fas fa-check"></i></button><button class="excluir" onclick="excluir('+ numlista +')"><i class="fas fa-times"></i></button></div>'
     texto_tarefa.value =""
+    numlista++
 })
-   
-tarefa.style.textDecoration = "line-through"
 function concluida(n){
+    tarefa[n].style.textDecoration = "line-through"
 
 }
+
+function excluir(n){
+   lista[n].style.display = "none"
+}
+
 
 
 
