9ca6ab80be80e030e25c086b3037c10c89bbcb6a
emerson da conceição cabral <emersoncabral202@gmail.com>
2021-06-16 12:21:43 -0300

    function para localstorage

--- lista de tarefa/lista_de_tarefa.js
+++ lista de tarefa/lista_de_tarefa.js
@@ -7,9 +7,16 @@ var numlista = 0
 
 var adicionar = document.querySelector(".adicionar").addEventListener("click", function (){
     conteiner_lista.innerHTML += '<div class="lista" name="lista"><div class="tarefa" name="tarefa">' + texto_tarefa.value + '</div><button class="concluida" onclick="concluida('+ numlista +')"><i class="fas fa-check"></i></button><button class="excluir" onclick="excluir('+ numlista +')"><i class="fas fa-times"></i></button></div>'
+    localStorage.setItem('id' + numlista, conteiner_lista.innerHTML)
     texto_tarefa.value =""
     numlista++
 })
+window.addEventListener("load", function(){
+    for(let i = 0;i < localStorage.length;i++){
+    conteiner_lista.innerHTML = localStorage.getItem("id" + i)
+}
+})
+
 function concluida(n){
     if(tarefa[n].style.textDecoration != "line-through"){
         tarefa[n].style.textDecoration = "line-through"
@@ -26,7 +33,10 @@ function concluida(n){
 }
 
 function excluir(n){
-   lista[n].style.display = "none"
+    lista[n].style.display = "none"
+    localStorage.removeItem("id" + n)
+
+
 }
 
 
