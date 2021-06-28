355c29a158e038301a40d5a26f0de38179d4b1db
emerson da conceição cabral <emersoncabral202@gmail.com>
2021-06-28 12:40:39 -0300

    ajuste

--- lista de tarefa/lista_de_tarefa.js
+++ lista de tarefa/lista_de_tarefa.js
@@ -5,37 +5,32 @@ var tarefa = document.getElementsByName("tarefa")
 
 var numlista = 0
 
+
+
 var adicionar = document.querySelector(".adicionar").addEventListener("click", function (){
     conteiner_lista.innerHTML += '<div class="lista" name="lista"><div class="tarefa" name="tarefa">' + texto_tarefa.value + '</div><button class="concluida" onclick="concluida('+ numlista +')"><i class="fas fa-check"></i></button><button class="excluir" onclick="excluir('+ numlista +')"><i class="fas fa-times"></i></button></div>'
-    localStorage.setItem('id' + numlista, conteiner_lista.innerHTML)
     texto_tarefa.value =""
     numlista++
 })
-window.addEventListener("load", function(){
-    for(let i = 0;i < localStorage.length;i++){
-    conteiner_lista.innerHTML = localStorage.getItem("id" + i)
-}
-})
 
-function concluida(n){
-    if(tarefa[n].style.textDecoration != "line-through"){
-        tarefa[n].style.textDecoration = "line-through"
-        tarefa[n].style.backgroundColor = "rgb(114, 230, 114)"
-        tarefa[n].style.color = "white"
+
+function concluida(index){
+    if(tarefa[index].style.textDecoration != "line-through"){
+        tarefa[index].style.textDecoration = "line-through"
+        tarefa[index].style.backgroundColor = "rgb(114, 230, 114)"
+        tarefa[index].style.color = "white"
 
     }else{
-        tarefa[n].style.textDecoration = "none"
-        tarefa[n].style.backgroundColor = "white"
-        tarefa[n].style.color = "black"
+        tarefa[index].style.textDecoration = "none"
+        tarefa[index].style.backgroundColor = "white"
+        tarefa[index].style.color = "black"
 
     }
 
 }
 
-function excluir(n){
-    lista[n].style.display = "none"
-    localStorage.removeItem("id" + n)
-
+function excluir(index){
+    lista[index].style.display = "none"
 
 }
 
